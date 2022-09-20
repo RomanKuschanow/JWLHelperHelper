00c1945abe6b899d9016d8666b0e54dd5c436a76
RomanKuschanow <kuschanow@gmail.com>
2022-09-20 17:37:31 +0300

    rename Halper to Helper

--- JWLHalperHalper/ViewModel.cs
+++ JWLHalperHalper/ViewModel.cs
@@ -7,7 +7,7 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
-namespace JWLHalperHalper
+namespace JWLHelperHelper
 {
     public class ViewModel : INotifyPropertyChanged
     {
